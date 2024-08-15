fcd3649e1247b5a4d2a38126483550c0e07b2967
Sidney <sidney.correia.cavalcanti@gmail.com>
2024-08-14 22:10:07 -0300

    atualização 2024

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Routes from './routes';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'jquery';
+import '@popperjs/core';
+
+
 
 import "./App.css"
 import  'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import 'bootstrap/dist/css/font-awesome.min.css';
+//import 'bootstrap/dist/css/font-awesome.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap/dist/js/jquery.min.js';
+//import 'bootstrap/dist/js/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap/dist/js/popper.js'
+//import 'bootstrap/dist/js/popper.js'
 
 
 
